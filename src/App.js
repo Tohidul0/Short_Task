28f0a67659acf50502458b4f54d5f613c75a5c2d
Tohidul0 <tohidulalam967418@gmail.com>
2024-05-01 11:34:59 +0600

    start with material ui,make based sructure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Grid } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 function App() {
+  const theme = useTheme();
+  const secondaryColor = '#17998C';
+  const whiteColor = '#ffffff';
+  const imageUrl = '/aiir.jpg';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Grid container spacing={2}>
+          {/* First Column - 1/6 Width */}
+          <Grid item xs={2}>
+            <div style={{ backgroundColor:secondaryColor, textAlign: 'center', height: '100vh',borderRadius: '10px' }}>1/6 Width</div>
+          </Grid>
+          {/* Second Column - 5/6 Width */}
+          <Grid item xs={10}>
+            <Grid container spacing={2}>
+              {/* First Row */}
+              <Grid item xs={12}>
+                <div style={{ backgroundColor: secondaryColor, textAlign: 'center', height: '15.33vh',borderRadius: '10px' }}>1/3 Height</div>
+              </Grid>
+              
+              {/* Second Row */}
+              <Grid item xs={12}>
+                {/* Image with fixed color overlay */}
+                <div style={{ position: 'relative', height: '63.33vh', borderRadius: '10px', overflow: 'hidden' }}>
+                  <img src={imageUrl} alt="background" style={{ width: '100%', height: '100%', objectFit: 'cover', filter: 'brightness(50%)', borderRadius: '10px' }} />
+                  <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: secondaryColor, opacity: 0.5 }}></div>
+                </div>
+              </Grid>
+              
+              {/* Third Row */}
+              <Grid item xs={12}>
+                <div style={{ backgroundColor: secondaryColor, textAlign: 'center', height: '15.33vh',borderRadius: '10px' }}>1/3 Height</div>
+              </Grid>
+              
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
